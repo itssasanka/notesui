34f8c3d47098a44e85f0d105f6c41a6530b002e7
Sasanka <itssasanka@gmail.com>
2017-02-20 17:04:42 +0530

    Create UI elements

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return [
+      {
+        id: 1,
+        title: "Note1",
+        content: "hello",
+        tags: ["test", "test2"]
+      },
+      {
+        id: 2,
+        title: "Note2",
+        content: "hi",
+        tags: ["test2", "test3"]
+      }
+    ];
+  }
 });
