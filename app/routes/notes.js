2f04b539bc79358449d6b20fbcf1762d5ea66314
Sasanka <itssasanka@gmail.com>
2017-03-12 18:10:36 +0530

    Fix UI

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -1,21 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model(){
-    return this.store.findAll('note');
-    // return [
-    //   {
-    //     id: 1,
-    //     title: "Note1",
-    //     content: "hello",
-    //     tags: ["test", "test2"]
-    //   },
-    //   {
-    //     id: 2,
-    //     title: "Note2",
-    //     content: "hi",
-    //     tags: ["test2", "test3"]
-    //   }
-    // ];
-  }
+
+	queryParams:{
+		tag_id: {
+			refreshModel: true
+		}
+	},	
+	model(params){
+		return this.get('store').query('note', params);
+	}
 });
--- app/serializers/note.js
+++ app/serializers/note.js
@@ -1,17 +1,22 @@
 import DS from 'ember-data';
 
-export default DS.RESTSerializer.extend({
-  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-    console.log(payload);
-    payload = {
-      notes: payload
-    };
+export default DS.RESTSerializer.extend(
+	DS.EmbeddedRecordsMixin, 
+	{
+		attrs: {
+			tags: { embedded: 'always' },
+		}
+	},
 
 
+	{
+		normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+			payload = {
+				notes: payload
+			};
 
-    console.log(payload);
-
-    return this._super(store, primaryModelClass, payload, id, requestType);
-  }
-
-});
+			console.log(payload);
+			return this._super(store, primaryModelClass, payload, id, requestType);
+		}
+	}
+);
