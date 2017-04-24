2cc61d87c62dc43546d3d7269efad64769ea63a9
Sasanka Panguluri <itssasanka@gmail.com>
2017-04-23 20:41:09 -0700

    temp

--- app/components/note-create.js
+++ app/components/note-create.js
@@ -1,11 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  create: false,
-  actions: {
-    toggleCreate(){
-      this.toggleProperty('create');
+    store: Ember.inject.service(),
+    create: false,
+    actions: {
+        toggleCreate(){
+            this.toggleProperty('create');
+        },
+        createNewNote(){
+            this.get('store').createRecord('note', {
+                    title: this.get('title'),
+                    content: this.get('body'),
+                    tag_ids: [1]
+                }
+            );
+        }
+
     }
-  }
 
 });
