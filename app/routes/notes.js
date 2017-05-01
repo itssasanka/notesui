ef3d4ee7497d65444466ef425fc29e1669c93990
Sasanka Panguluri <itssasanka@gmail.com>
2017-04-30 18:57:35 -0700

    separate tags

--- app/routes/notes.js
+++ app/routes/notes.js
@@ -2,12 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-	queryParams:{
-		tag_id: {
-			refreshModel: true
-		}
-	},	
-	model(params){
-		return this.get('store').query('note', params);
-	}
+    queryParams: {
+        tag_id: {
+            refreshModel: true
+        }
+    },
+    model(params){
+        return Ember.RSVP.hash({
+            notes: this.store.query('note', params),
+            tags: this.store.findAll('tag')
+        });
+    },
+
+    setupController(controller, model) {
+        this._super(...arguments);
+        Ember.set(controller, 'notes', model.notes);
+        Ember.set(controller, 'tags', model.tags);
+    }
 });
