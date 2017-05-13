f46036e296de0fe1bb78c358c7d679e740f274ce
Sasanka Panguluri <itssasanka@gmail.com>
2017-05-13 08:42:23 -0700

    Error functions

--- app/components/tag-create.js
+++ app/components/tag-create.js
@@ -1,19 +1,27 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    checkData(){
+        if(Ember.isBlank(this.get('name'))){
+            this.errors.pushObject({name: 'Tag name cannot be empty.'});
+        }
+    },
 
     store: Ember.inject.service(),
     create: false,
+    errors: [],
     actions: {
         toggleCreate(){
             this.toggleProperty('create');
         },
         createNewTag(){
-            let tag = this.get('store').createRecord('tag', {
-                name: this.get('name'),
-            });
-            tag.save();
+            this.checkData();
+            if(Ember.isEmpty(this.get('errors'))){
+                let tag = this.get('store').createRecord('tag', {
+                    name: this.get('name'),
+                });
+                tag.save();
+            }
         },
-
     }
 });
