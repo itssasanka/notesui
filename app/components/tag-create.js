bb7d175cf2fdd6689b8b1478db0e941424173cc2
Sasanka Panguluri <itssasanka@gmail.com>
2017-07-15 16:39:36 -0700

    changes

--- app/components/tag-create.js
+++ app/components/tag-create.js
@@ -1,34 +1,40 @@
-import Ember from 'ember';
+import Ember from 'ember'
 
 export default Ember.Component.extend({
-    checkData(){
-        if(Ember.isBlank(this.get('name'))){
-            this.errors.pushObject({name: 'Tag name cannot be empty.'});
-        }
-    },
-    didUpdateAttrs(){
-      console.log("updated");
-    },
-
-    store: Ember.inject.service(),
-    create: false,
-    errors: [],
-    actions: {
-        toggleCreate(){
-            this.toggleProperty('create');
-        },
-        createNewTag(){
-            this.checkData();
-            if(Ember.isEmpty(this.get('errors'))){
-                let tag = this.get('store').createRecord('tag', {
-                    name: this.get('name'),
-                });
-                tag.save().then(()=>{}).catch((aa)=>{this.set('errors', aa.errors.map(e=>e.detail))});
-
-            }
-        },
-        toggleSubmit(){
-            this.$('.small-button').prop('disabled', Ember.isBlank(this.get('name')));
-        }
+  checkData(){
+    if(Ember.isBlank(this.get('name'))){
+      this.errors.pushObject('Tag name cannot be empty.');
     }
+  },
+
+  store: Ember.inject.service(),
+  create: false,errors: [],
+  actions: {
+    toggleCreate(){
+      this.set('errors', []);
+      this.toggleProperty('create');
+    },
+
+    createNewTag(){
+      this.set('errors', []);
+      this.checkData();
+      if(Ember.isEmpty(this.get('errors'))){
+        let tag = this.get('store').createRecord('tag', {
+          name: this.get('name'),
+        });
+        tag.save()
+          .then(()=>{
+            this.set('name','');
+            this.toggleProperty('create')
+          })
+          .catch((response)=>{
+            this.set('errors', response.errors.map(e=>e.detail))
+          });
+      }
+    },
+
+    toggleSubmit(){
+      this.$('.small-button').prop('disabled', Ember.isBlank(this.get('name')));
+    }
+  }
 });
